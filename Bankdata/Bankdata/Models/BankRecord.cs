43099794173e670f36a57571591c200a9f5e0e10
chandra6900 <chandra6900@gmail.com>
2017-12-21 12:53:11 +0530

    ModelRecord added
    initial commit model record

---
+++ Bankdata/Bankdata/Models/BankRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bankdata.Models
+{
+    public class BankRecord
+    {
+        public string BankName { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+    }
+}
